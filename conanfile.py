05390eb10592dcc7f4f6b0e4cd1c3802e89917f1
dePaul Miller <depaulmillz@users.noreply.github.com>
2023-03-05 15:29:48 -0500

    Updates to the build and readme

--- conanfile.py
+++ conanfile.py
@@ -20,7 +20,7 @@ class LSlabConan(ConanFile):
 
     def configure(self):
         if self.options.cuda_arch == None:
-            self.options.cuda_arch = '70;75;80;86'
+            self.options.cuda_arch = '70;75;80;86;89;90'
         if self.options.cuda_compiler == None:
             self.options.cuda_compiler = "nvcc"
 
